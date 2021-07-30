97b3ff6ad8a83b4067dec74c3fca0593bb73d84c
sudheerb9 <bds15@iitbbs.ac.in>
2021-07-30 20:50:00 +0530

    ok

--- components/Principal/PrincipalElement.js
+++ components/Principal/PrincipalElement.js
@@ -22,7 +22,7 @@ function Principal (){
                     <Col sm={12} md={9} lg={9} xs={12} style={{marginTop : '10px'}}>
                       <h4 align="center">Mfghjj.Rykknjnhh</h4>
                       <h6 align="center">fghj,ghhjjj</h6>
-                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                     </Col>
                 </Row>
             </Container>
