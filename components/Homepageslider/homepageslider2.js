6279aab74fced81a3d428d4b509f03c1c66c026f
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-07-30 22:15:55 +0530

    Update homepageslider2.js

--- components/Homepageslider/homepageslider2.js
+++ components/Homepageslider/homepageslider2.js
@@ -1,33 +1,80 @@
 import React, { useState } from 'react';
-
-import { UncontrolledCarousel, Row, Col } from "reactstrap";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from 'reactstrap';
+import Image from 'next/image'
 
 const items = [
   {
-    src: require("images/slider/3.jpg"),
-    altText: "Slide 1",
-    caption: "",
-    header: "",
-    key: "1",
+    src: '../../../public/images/slider/3.jpg',
+    altText: 'Slide 1',
   },
   {
-    src: require("images/slider/3.jpg"),
-    altText: "Slide 2",
-    caption: "",
-    header: "",
-    key: "2",
+    src: '../../..//public/images/slider/3.jpg',
+    altText: 'Slide 2',
+    caption: 'Slide 2'
   },
 
 ];
 
-const Homepageslider = () => (
-  <Row>
-    <Col md="12" className="mx-auto">
-      <UncontrolledCarousel items={items} />
-    </Col>
-  </Row>
-);
+const Homepageslider = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
 
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+        <CarouselItem
+            onExiting={() => setAnimating(true)}
+            onExited={() => setAnimating(false)}
+            key={item.src}
+        >
+          <Image src={item.src} alt={item.altText} width="100%" height="100%"/>
+          {/*<div className={'carousel-caption fvgb d-none d-md-block'}>*/}
+          {/*  <h5 className={'animated bounceInDown cartext' }>Create an Awesome Website Today </h5>*/}
+          {/*  <p className={'animated bounceInLeft cartext'}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam justo neque, <br/>*/}
+          {/*    aliquet sit amet elementum vel, vehicula eget eros. Vivamus arcu metus, mattis <br/>*/}
+          {/*    sed sagittis at, sagittis quis neque. Praesent.</p>*/}
+          {/*  <div className={'row vbh'}>*/}
+          {/*    <div className={'btn btn-success animated bounceInUp'}> Apply Online</div>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+        </CarouselItem>
+    );
+  });
+
+  return (
+      <Carousel
+          activeIndex={activeIndex}
+          next={next}
+          previous={previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+      </Carousel>
+  );
 }
 
 export default Homepageslider;
